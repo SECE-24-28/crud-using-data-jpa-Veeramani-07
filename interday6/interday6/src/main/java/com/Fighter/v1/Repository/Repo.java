0037b983579ec5b830f35fdd29bc2f70ccff03af
Veeramani-07 <veeramani.k2024ece@sece.ac.in>
2026-06-07 09:30:26 +0530

    Delete interday6/interday6 directory

--- interday6/interday6/src/main/java/com/Fighter/v1/Repository/Repo.java
+++
@@ -1,7 +0,0 @@
-package com.Fighter.Dubakur.Repository;
-
-import com.Fighter.Dubakur.Model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repo extends JpaRepository<Student, Integer> {
-}
